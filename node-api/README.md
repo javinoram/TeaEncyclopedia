16368a3f9b8a0b7be8e80c0ecef6e73e72f064b7
jnorambu <javinorambl@gmail.com>
2024-05-22 15:53:35 -0400

    Creating GET-all and POST-insert-tea method

---
+++ node-api/README.md
@@ -0,0 +1,8 @@
+# Tea Api
+The Api use as basis *NodeJs* and *Express*, the SQLite3 database is in the *sql-database*.
+
+### Get routes
+1. */get-teas* return all the teas available in the database.
+
+### Post routes
+1. */add-new-tea* route that allow to add a new tea in the database, the expected object new to have *{"tea_name": "", "type": "", "country": "", "region": ""}*
--- node-api/server.js
+++ node-api/server.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const port = 8080;
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
+const url_database = '../sql-database/tea.db'
+app.use( express.json() );
+
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-  })
+
+//Route to get all the teas
+app.get('/get-teas', (req, res) => {
+  let db = new sqlite3.Database(url_database, (err) => { if (err) { return console.error(err.message); } });
+  let sql_command = 'SELECT t.* FROM tea t;';
+  
+  return new Promise( (resolve) => {
+    db.all( sql_command, [], (err, row) => {resolve(row);})
+  }).then( (row) => {
+    db.close()
+    return res.json({"data": row}); 
+  }).catch( (err) => {
+    db.close();
+    return console.log(err);
+  });
+
+});
+
+//Route to add a new tea (the api should receive a json with the data)
+app.post('/add-new-tea', (req, res) => {
+  let db = new sqlite3.Database(url_database, (err) => { if (err) { return console.error(err.message); } });
+  let sql_command = 'INSERT INTO tea (tea_name, type, country, region) VALUES ((?), (?), (?), (?))';
+  let data = req.body;
+
+  return new Promise( (resolve) => {
+    db.run(sql_command, [data['tea_name'], data['type'], data['country'], data['region']], (err, row)=>{ resolve(row); });
+  }).then( (row) => {
+      db.close();
+      return res.json({});
+  }).catch( (err)=> {
+      db.close();
+      return res.json({});
+  });
+});
